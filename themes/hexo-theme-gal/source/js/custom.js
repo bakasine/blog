4ce7a2cf4fcd5502ef34ff7866f08ea814510d9c
wesker <hentai121@163.com>
2022-09-13 00:16:23 +0800

    add style

--- themes/hexo-theme-gal/source/js/custom.js
+++ themes/hexo-theme-gal/source/js/custom.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: wesker
+ * @Date: 2022-09-12 16:59:50
+ * @FilePath: \hexo-theme-gal\source\js\custom.js
+ * Copyright (c) 2022 by wesker hentai121@163.com, All Rights Reserved. 
+ */
 /*页面载入完成后，创建复制按钮*/
 !function (e, t, a) { 
     /* code */
@@ -14,5 +20,4 @@
         });
     }
     initCopyCode();
-    }(window, document);
-
+}(window, document);
